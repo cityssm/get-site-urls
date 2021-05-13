3d9c28f172de81a8aaaf8dbce0ab36c61f168aa9
dangowans <d.gowans@cityssm.on.ca>
2021-05-13 13:43:07 -0400

    increased coverage for errors

--- test/test.js
+++ test/test.js
@@ -5,6 +5,10 @@ const index_js_1 = require("../index.js");
 describe("getSiteUrls()", () => {
     it("Has results for saultstemarie.ca", async () => {
         const urls = await index_js_1.getSiteUrls("https://saultstemarie.ca");
-        assert.ok(urls);
+        assert.ok(urls.pages.length > 0);
+    });
+    it("Has results for saultstemarie.ca/Broken-Link.aspx", async () => {
+        const urls = await index_js_1.getSiteUrls("https://saultstemarie.ca/Broken-Link-" + Date.now().toString() + ".aspx");
+        assert.ok(urls.errors.length > 0);
     });
 });
--- test/test.ts
+++ test/test.ts
@@ -6,6 +6,11 @@ describe("getSiteUrls()", () => {
 
   it("Has results for saultstemarie.ca", async() => {
     const urls = await getSiteUrls("https://saultstemarie.ca");
-    assert.ok(urls);
+    assert.ok(urls.pages.length > 0);
+  });
+
+  it("Has results for saultstemarie.ca/Broken-Link.aspx", async() => {
+    const urls = await getSiteUrls("https://saultstemarie.ca/Broken-Link-" + Date.now().toString() + ".aspx");
+    assert.ok(urls.errors.length > 0);
   });
 });
