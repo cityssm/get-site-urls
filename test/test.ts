fc60d12ddf72257fe1cf8a2e4498a577f681c2be
dangowans <d.gowans@cityssm.on.ca>
2022-02-01 15:49:42 -0500

    add gflgardens test

--- test/test.ts
+++ test/test.ts
@@ -14,6 +14,11 @@ describe("getSiteUrls()", () => {
     assert.ok(urls.pages.length > 0);
   });
 
+  it("Has results for gflgardens.ca", async() => {
+    const urls = await getSiteUrls("http://gflgardens.ca");
+    assert.ok(urls.pages.length > 0);
+  });
+
   it("Has errors for saultstemarie.ca/Broken-Link.aspx", async() => {
     const urls = await getSiteUrls("https://saultstemarie.ca/Broken-Link-" + Date.now().toString() + ".aspx");
     assert.ok(urls.errors.length > 0);
