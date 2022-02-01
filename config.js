31c0997f174287ea1df66466204bb9f0ebea84fe
dangowans <d.gowans@cityssm.on.ca>
2022-02-01 15:49:25 -0500

    tweak http agent

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+import http from "http";
 export const throttleSleepMillis = 200;
 export const maxRequestCount = 5;
 export const axios_requestTimeoutMillis = 3 * 60000;
-export const axios_httpAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
+export const axios_httpAgent = new http.Agent({
+    keepAlive: true
+});
--- config.ts
+++ config.ts
@@ -1,5 +1,9 @@
+import http from "http";
+
 export const throttleSleepMillis = 200;
 export const maxRequestCount = 5;
 
 export const axios_requestTimeoutMillis = 3 * 60_000;
-export const axios_httpAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
+export const axios_httpAgent = new http.Agent({
+  keepAlive: true
+});
